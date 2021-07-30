ad96a7139580bf7efe7aa680957d9dd76381efa2
Artemy <artemiy.morozov.00@mail.ru>
2021-07-30 03:13:46 +0300

    30.07.21

--- mysite/app/mixins.py
+++ mysite/app/mixins.py
@@ -1,11 +1,23 @@
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic import View
 
-from .models import Category, Cart, Customer
+from .models import Category, Cart, Customer, Notebook, Smartphone
+
 
 class CategoryDetailMixin(SingleObjectMixin):
 
+    CATEGORY_SLUG_TO_PRODUCT_MODEL = {
+        'notebooks': Notebook,
+        'smartphones': Smartphone
+    }
+
     def get_context_data(self, **kwargs):
+        if isinstance(self.get_object(), Category):
+            model = self.CATEGORY_SLUG_TO_PRODUCT_MODEL[self.get_object().slug]
+            context = super().get_context_data(**kwargs)
+            context['categories'] = Category.objects.get_categories_for_category_menu()
+            context['category_products'] = model.objects.all()
+            return context
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.get_categories_for_category_menu()
         return context
