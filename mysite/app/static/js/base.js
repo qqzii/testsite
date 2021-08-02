42823a4899c265c94d40697ce01906dd53cbb8a8
Artemy <artemiy.morozov.00@mail.ru>
2021-08-02 03:23:36 +0300

    02.08.21

--- mysite/app/static/js/base.js
+++ mysite/app/static/js/base.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event){
-        $('.header__burger, .header__menu').toggleClass('active');
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active')
     })
-})
+});
